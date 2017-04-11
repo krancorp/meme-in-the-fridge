6a380200670a3bbfff384ba9ab1a352b1f39d357
watzkuh <kai@naschinski.de>
2017-04-11 13:03:40 +0200

    initializing via command line arguments + add comfort-scripts

---
+++ fridge-sensor/start.sh
@@ -0,0 +1,5 @@
+# expects target ip address:port as argument, example : "127.0.0.1:8080"
+./fridge-sensor $1 Pfungstaedter &
+./fridge-sensor $1 Krombacher &
+./fridge-sensor $1 Aepfel &
+./fridge-sensor $1 Salz &
