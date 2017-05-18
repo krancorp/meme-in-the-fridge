b1edef550971804f110d978ca68d49d2cbfac00d
Jakob <dev@schrej.net>
2017-05-18 12:13:35 +0200

    remove one useless thread from fridge server
    This fix will greatly improve the performance of the rest of the system as the scheduler doesn't have to count as high to iterate through all running threads.

--- fridge-server/fridge-server.go
+++ fridge-server/fridge-server.go
@@ -5,11 +5,10 @@ var lastEntries []string
 func main() {
 	configPath := "./config.json"
 	fridgeStock, tableHeader := readConfig(configPath)
-	block := make(chan bool)
+	
 	go ShoppingWrapper(fridgeStock)
 	go startUdpServer(fridgeStock)
-	go startHttpServer(fridgeStock, tableHeader)
-	<- block
+	startHttpServer(fridgeStock, tableHeader)
 }
 
 
